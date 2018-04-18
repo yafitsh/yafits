e6ddcd261a98a875e4c053f6cbd76264652873f7
Yafit Shitrit <yafitmir@gmail.com>
2018-04-18 14:53:14 +0300

    change the remote method and the db file

--- common/models/payment.js
+++ common/models/payment.js
@@ -2,18 +2,25 @@
 
 module.exports = function(Payment) {
 
-    Payment.paymentBetweenDates = function(fromDate, toDate, cb) {
-        var payments = [];
-        var newFromDate = new Date(fromDate);
-        var newToDate = new Date(toDate);
+    Payment.paymentBetweenDates = function(p_date_start, p_date_end, cb) {
+        var payments_list = [];
+        var newp_date_start = formatDate(p_date_start);
+        var newp_date_end = formatDate(p_date_end);
         var myJson = require('../../mydata.json').models.Payment;
         Object.keys(myJson).forEach(function(key){
             var obj = JSON.parse(myJson[key]);
-            var date = new Date(obj.date);  
-            if (date >= newFromDate && date <= newToDate){
-                   payments.push(obj);
+            var date = formatDate(obj.date);  
+            if (date >= newp_date_start && date <= newp_date_end){
+                payments_list.push(obj);
             } 
         })
-        cb(null, payments);   
-    }
+        cb(null, payments_list);   
+    };
+
+
+    var formatDate = function(date){
+        var dateArr = date.split('-');
+        var newDate = new Date(dateArr[2],dateArr[1],dateArr[0]);
+        return newDate; 
+    } 
 };   
