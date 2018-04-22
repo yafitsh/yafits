02b44ffaf1992ccfbde5f94a8f61906de095b3ba
Yafit Shitrit <yafitmir@gmail.com>
2018-04-22 15:28:30 +0300

    change the remote method and the db file

--- common/models/payment.js
+++ common/models/payment.js
@@ -2,25 +2,34 @@
 
 module.exports = function(Payment) {
 
-    Payment.paymentBetweenDates = function(p_date_start, p_date_end, cb) {
+    Payment.paymentBetweenDates = function(p_date_start, p_date_end, auth_code, tz, cb) {
         var payments_list = [];
-        var newp_date_start = formatDate(p_date_start);
-        var newp_date_end = formatDate(p_date_end);
         var myJson = require('../../mydata.json').models.Payment;
         Object.keys(myJson).forEach(function(key){
             var obj = JSON.parse(myJson[key]);
-            var date = formatDate(obj.date);  
-            if (date >= newp_date_start && date <= newp_date_end){
+            if (!tz){  
+                var newp_date_start = formatDate(p_date_start);
+                var newp_date_end = formatDate(p_date_end);
+                var date = formatDate(obj.date);
+                if (date >= newp_date_start && date <= newp_date_end && !auth_code){
+                    payments_list.push(obj);
+                } 
+                else if (date >= newp_date_start && date <= newp_date_end && obj.auth_code == auth_code){
+                    payments_list.push(obj);
+                }
+            }
+            else if (tz == obj.tz){
                 payments_list.push(obj);
-            } 
+            }
         })
         cb(null, payments_list);   
     };
-
 
     var formatDate = function(date){
         var dateArr = date.split('-');
         var newDate = new Date(dateArr[2],dateArr[1],dateArr[0]);
         return newDate; 
-    } 
+    }
+    
+    
 };   
