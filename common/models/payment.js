7edef905be3d2c9b187b6efd584e50a951cf0bb7
Yafit Shitrit <yafitmir@gmail.com>
2018-04-24 16:54:37 +0300

    change the payment.js file

--- common/models/payment.js
+++ common/models/payment.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = function(Payment) {
-
-    Payment.paymentBetweenDates = function(p_date_start, p_date_end, auth_code, tz, cb) {
+    Payment.paymentBetweenDates = function(p_date_start, p_date_end, auth_code, tz, office_code, cb) {
         var payments_list = [];
+        var filterd_payments_list = [];
         var myJson = require('../../mydata.json').models.Payment;
+       
         Object.keys(myJson).forEach(function(key){
             var obj = JSON.parse(myJson[key]);
             if (!tz){  
@@ -20,9 +21,21 @@ module.exports = function(Payment) {
             }
             else if (tz == obj.tz){
                 payments_list.push(obj);
-            }
+            }      
         })
-        cb(null, payments_list);   
+
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        var request = require('sync-request');
+        var res = request('GET', 'https://192.168.0.111/gov/sb/officeauthorizer/allowedAuthorities?officeCode='+office_code);
+        var codeArr = JSON.parse(res.getBody());
+        if (codeArr.length != 0){
+            for (var i = 0; i < payments_list.length; ++i ){
+                if (codeArr.includes(payments_list[i].auth_code)){
+                    filterd_payments_list.push(payments_list[i]);
+                }
+            }
+        }   
+        cb(null, filterd_payments_list); 
     };
 
     var formatDate = function(date){
@@ -31,5 +44,7 @@ module.exports = function(Payment) {
         return newDate; 
     }
     
-    
 };   
+
+
+
